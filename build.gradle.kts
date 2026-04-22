362de010b367ae80554414c3e60a19a629875a5f
gul <guldotkay@gmail.com>
2026-04-23 00:35:15 +0530

    created Gusher kmp library project

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.library) apply false
+}
+
+allprojects {
+    group = "io.gusher"
+    version = "0.1.0-SNAPSHOT"
+}
